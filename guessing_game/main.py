9185317225d9238d4b4330500adc3c9fe66cbd86
nilesh <manenilesh912@gmail.com>
2025-06-05 16:24:41 +0530

    Updated files

--- guessing_game/main.py
+++ guessing_game/main.py
@@ -10,20 +10,30 @@ def nuber_guessing_game():
 
     while True:
 
-        user_guess = int(input("Take a Guess:- "))
-        guesses_taken += 1
+        try:
+            user_guess_str = input("Take a guess: ")
+            user_guess = int(user_guess_str)
+            guesses_taken += 1
 
-        if user_guess < secret_number:
-            print("Your guess is too low! Try again.")
+            if user_guess < secret_number:
+                print("Your guess is too low! Try again.")
+            
+            elif user_guess > secret_number:
+                print("Your guess is too high! Try again.")
+
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} correctly!")
+                print(f"It took you {guesses_taken} guess ")
+
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
         
-        elif user_guess > secret_number:
-            print("Your guess is too high! Try again.")
-
-        else:
-            print(f"Congratulations! You guessed the number {secret_number} correctly!")
-            print(f"It took you {guesses_taken} guess ")
-
-            break
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            continue
 
 
 if __name__=="__main__":
